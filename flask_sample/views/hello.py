79d0e62992b0a412cbba3999835cd4c20c14ba5c
suchithbalne <suchith.sameri@gmail.com>
2023-11-13 13:51:42 -0500

    Login and register usng Flask

--- flask_sample/views/hello.py
+++ flask_sample/views/hello.py
@@ -1,16 +1,8 @@
-import os
-
-from flask import Blueprint, Flask, request
-
-app = Flask(__name__)
-#sample = Blueprint('hello', __name__, url_prefix='/')
+from flask import Blueprint, request
+hello = Blueprint('hello', __name__, url_prefix='/')
 
 
-@app.route('/')
-def hello_world():
+@hello.route('/')
+def index():
     name = request.args.get('name', 'World')
     return f'Hello {name}!'
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
