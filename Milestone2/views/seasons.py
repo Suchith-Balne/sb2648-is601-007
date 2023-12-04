15c8d21c4963f389096fc507009713a8efa79f39
suchithbalne <suchith.sameri@gmail.com>
2023-12-04 18:38:17 -0500

    added code for edit seasons

--- Milestone2/views/seasons.py
+++ Milestone2/views/seasons.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template, jsonify
 from sql.db import DB
-from flask import flash
+from flask import Blueprint, redirect, render_template, request, flash, url_for
 seasons = Blueprint('seasons', __name__, url_prefix='/seasons')
 
 @seasons.route('/list', methods=["GET"])
-def get_episodes():
+def get_seasons():
     try:
         # Replace "SELECT * FROM episodes" with your actual query
         result = DB.selectAll("SELECT * FROM seasons")
@@ -16,3 +16,135 @@ def get_episodes():
     except Exception as e:
         flash("Error occured" + str(e), "error")
     return render_template("list_seasons.html", rows=rows)
+
+@seasons.route('/add', methods=["GET", "POST"])
+def add_season():
+    if request.method == "POST":
+        season_id = request.form.get("season_id")
+        poster_url = request.form.get("poster_url")
+        name = request.form.get("name")
+        overview = request.form.get("overview")
+        number = request.form.get("number")
+        air_date = request.form.get("air_date")
+        episode_count = request.form.get("episode_count")
+        has_error = False
+        
+        if not season_id:
+            flash("season id is required.", "danger")
+            has_error = True
+        
+        if not poster_url:
+            flash("poster_url is required.", "danger")
+            has_error = True
+            
+        if not name:
+            flash("name is required.", "danger")
+            has_error = True
+        
+        if not overview:
+            flash("overview is required.", "danger")
+            has_error = True
+        
+        if not number:
+            flash("number is required.", "danger")
+            has_error = True
+        
+        if not air_date:
+            flash("air date is required.", "danger")
+            has_error = True
+
+        if not episode_count:
+            flash("Episode count is required.", "danger")
+            has_error = True
+        
+        if not has_error:
+            try:
+                result = DB.insertOne("""
+                INSERT INTO seasons (id, poster_url, name, overview, number, air_date, episode_count)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                """,  *(season_id, poster_url, name, overview, number, air_date, episode_count)
+                )
+                if result.status:
+                    print("Season record created")
+                    flash("Created Season Record", "success")
+            except Exception as e:
+                print(f"insert error {e}")
+                flash("An error occurred while creating the season record. Please try again later.", "danger")
+    return render_template("manage_seasons.html",season=request.form)
+
+@seasons.route("/edit", methods=["GET", "POST"])
+def edit_season():
+    
+    row = {}
+    
+    season_id = request.args.get('id')
+    if not season_id:
+        flash("Missing season ID. Unable to edit.", "danger")
+    else:
+        if request.method == "POST":
+            season_id = request.form.get("season_id")
+            poster_url = request.form.get("poster_url")
+            name = request.form.get("name")
+            overview = request.form.get("overview")
+            number = request.form.get("number")
+            air_date = request.form.get("air_date")
+            episode_count = request.form.get("episode_count")
+            has_error = False
+            
+            if not season_id:
+                flash("season id is required.", "danger")
+                has_error = True
+            
+            if not poster_url:
+                flash("poster_url is required.", "danger")
+                has_error = True
+                
+            if not name:
+                flash("name is required.", "danger")
+                has_error = True
+            
+            if not overview:
+                flash("overview is required.", "danger")
+                has_error = True
+            
+            if not number:
+                flash("number is required.", "danger")
+                has_error = True
+            
+            if not air_date:
+                flash("air date is required.", "danger")
+                has_error = True
+
+            if not episode_count:
+                flash("Episode count is required.", "danger")
+                has_error = True
+            
+            if not has_error:
+                try:
+                    result = DB.update("""
+                                        UPDATE seasons
+                                        SET poster_url = %s, name = %s, overview = %s, number = %s, air_date = %s, episode_count = %s
+                                        WHERE id = %s
+                                        """, (poster_url, name, overview, number, air_date, episode_count, season_id))
+                    if result.status:
+                        print("Season record updated")
+                        flash("Updated Season Record", "success")
+                        return redirect(url_for("seasons.get_seasons"))
+                    else:
+                        flash("An error occurred while updating the season record. Please try again later.", "danger")
+                        return redirect(url_for("seasons.get_seasons"))
+                except Exception as e:
+                    print(f"Update error {e}")
+                    flash("An error occurred while updating the season record. Please try again later.", "danger")
+                    return redirect(url_for("seasons.get_seasons"))
+        try:
+            result = DB.selectOne("""SELECT * FROM seasons
+            WHERE id = %s"""
+            , season_id)
+            
+            if result.status:
+                row = result.row
+        except Exception as e:
+            flash("Error occured" + str(e), "error")
+    return render_template("manage_seasons.html",season=row)
+                
